8b268a22a22ec83eec3c3c8ae016255bc3531e5a
edev87 <ernestdicks06@gmail.com>
2022-06-18 18:43:19 -0500

    First commit, finished app

---
+++ src/components/Trending.jsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+
+function Trending() {
+    const [trending, setTrending] = useState([])
+    const url = 'https://api.coingecko.com/api/v3/search/trending';
+
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setTrending(response.data.coins); 
+            console.log(response.data.coins)
+        })
+    }, []);
+
+    return (
+        <div className='rounded-div my-12 py-8 text-primary'>
+            <h1 className='text-2xl font-bold py-4'>Trending Coins</h1>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {trending.map((coin, idx) => (
+                    <div key={idx} className="rounded-div flex justify-between p-4 hover:sclae-105 easy-in-out direction-300">
+                        <div className='flex w-full items-center justify-between'>
+                            <div className='flex'>
+                                <img className='mr-4 rounded-full' src={coin.item.small} alt="/"/>
+                                <div>
+                                <p className='font-bold'>{coin.item.name}</p>
+                                <p>{coin.item.symbol}</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center'>
+                        <img 
+                        src="w-4 mr-2" 
+                        alt=""/>
+                        <p>{coin.item.price_btc.toFixed(7)}</p>
+                    </div>
+            </div>
+            </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Trending
